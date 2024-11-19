4024490a1d1e7987288363a789dafbea122f078d
JuSeRDev <juserdev@gmail.com>
2024-11-19 12:04:59 -0500

    challenge terminado

---
+++ script.js
@@ -0,0 +1,35 @@
+const allRead = document.querySelector(".all-read")
+const count = document.querySelector(".count")
+const notification = document.querySelectorAll(".notification")
+const notificationActive = document.querySelectorAll(".notification-active")
+const classActive = "notification-active"
+let countArray = []
+
+allRead.addEventListener("click", ()=>{
+    count.innerHTML = "0"
+    clearBackground(notification)
+})
+
+function clearBackground (param){
+    param.forEach(noti => {
+        noti.classList.remove(classActive)
+    });
+}
+
+const post = document.querySelectorAll(".post")
+
+
+notification.forEach(noti => {
+    if(noti.classList.contains(classActive)){
+        countArray.push(1)
+    }
+    noti.addEventListener("click", ()=>{
+        if (noti.classList.contains(classActive)) {
+            noti.classList.remove(classActive)
+            countArray.pop()
+            count.innerHTML = `${countArray.length}`
+        }
+    })
+});
+
+
